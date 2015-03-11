f1e81397360ab3448243dc27ecc9cf90f90157ea
Gopal28 <gopalakrishnan@arivuventures.com>
2015-03-11 16:41:06 +0530

    Added image View and text
    Image and text in gridview ..

--- app/src/main/java/com/feed/arivusitrus/kitchenapp/MainActivity.java
+++ app/src/main/java/com/feed/arivusitrus/kitchenapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.feed.arivusitrus.kitchenapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,52 +11,23 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
 
-
-    GridView grid;
-    String[] web = {
-            "Google",
-            "Github",
-            "Instagram",
-            "Facebook",
-            "Flickr",
-            "Pinterest",
-            "Quora",
-            "Twitter",
-            "Vimeo",
-            "WordPress",
-            "Youtube",
-            "Stumbleupon",
-            "SoundCloud",
-            "Reddit",
-            "Blogger"
-    } ;
-    int[] imageId = {
-            R.drawable.ic_launcher,
-            R.drawable.ic_launcher,
-            R.drawable.ic_launcher,
-
-            R.drawable.ic_launcher,
-            R.drawable.ic_launcher,
-            R.drawable.ic_launcher,
-
-
-    };
+    GridView gv;
+    Context context;
+    ArrayList prgmName;
+    public static String [] prgmNameList={"Let Us C","c++","JAVA"};
+    public static int [] prgmImages={R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomGrid adapter = new CustomGrid(MainActivity.this, web, imageId);
-        grid=(GridView)findViewById(R.id.grid);
-        grid.setAdapter(adapter);
-        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Toast.makeText(MainActivity.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
-            }
-        });
+        gv=(GridView) findViewById(R.id.gridView1);
+        gv.setAdapter(new CustomAdapter(this, prgmNameList,prgmImages));
     }
+
+
 }
