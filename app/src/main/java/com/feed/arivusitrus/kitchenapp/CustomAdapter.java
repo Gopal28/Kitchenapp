98b829e91c80a41a8653bdb712786df606b4a4ce
Gopal28 <gopalakrishnan@arivuventures.com>
2015-03-11 19:59:58 +0530

    Created Button in Custom Grid View
    Created Button in Custom Grid View and test Toast function for working
    that button Checked..

--- app/src/main/java/com/feed/arivusitrus/kitchenapp/CustomAdapter.java
+++ app/src/main/java/com/feed/arivusitrus/kitchenapp/CustomAdapter.java
@@ -4,6 +4,7 @@ package com.feed.arivusitrus.kitchenapp;
  * Created by Arivu Sitrus on 11-03-2015.
  */
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,42 +20,64 @@ public class CustomAdapter extends BaseAdapter{
     private MainActivity context;
     private String[] items;
     LayoutInflater inflater;
+    String [] result;
     public CustomAdapter(MainActivity context, String[] items) {
         this.context = context;
         this.items = items;
+        result=items;
         inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
-
-    public View getView(final int position, View convertView, ViewGroup parent) {
-
-        if (convertView == null) {
-            convertView = inflater.inflate(R.layout.grid_single, null);
-        }
-        Button button = (Button) convertView.findViewById(R.id.grid_item);
-        button.setText(items[position]);
-        button.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                context.itemClicked(position);
-            }
-        });
-
-        return convertView;
+    public class Holder
+    {
+        TextView tv;
+        Button img;
     }
 
-    @Override
+
+
     public int getCount() {
         return items.length;
     }
 
-    @Override
+
     public Object getItem(int position) {
         return items[position];
     }
 
-    @Override
+
     public long getItemId(int position) {
         return position;
     }
+
+
+
+public View getView(final int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        Holder holder=new Holder();
+        View rowView;
+
+        rowView = inflater.inflate(R.layout.grid_single, null);
+       // holder.tv=(TextView) rowView.findViewById(R.id.textView1);
+        holder.img=(Button) rowView.findViewById(R.id.grid_item);
+
+       // holder.tv.setText(result[position]);
+    holder.img.setText(result[position]);
+// holder.img.setBackgroundColor(Color.RED);
+
+
+
+
+
+    rowView.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(context, "You Clicked "+result[position], Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return rowView;
+    }
+
 }
--- app/src/main/java/com/feed/arivusitrus/kitchenapp/MainActivity.java
+++ app/src/main/java/com/feed/arivusitrus/kitchenapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,9 +21,10 @@ public class MainActivity extends Activity {
 
     GridView gv;
     Context context;
+    Button delivery;
     ArrayList prgmName;TextView text;
-    private final String[] items = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private final String[] items = new String[]{"T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10",
+           "T11","T12" };
 
     public static String [] prgmNameList={"Let Us C","c++","JAVA"};
     public static int [] prgmImages={R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher};
@@ -30,20 +32,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         gv=(GridView) findViewById(R.id.gridView1);
+        //delivery=(Button)findViewById(R.id.item_delivery);
+      /*  delivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Toast.makeText(context, "You Clicked HOME DELIVERY/PARCEL ", Toast.LENGTH_LONG).show();
+            }
+        });*/
+
         CustomAdapter gridAdapter = new CustomAdapter(MainActivity.this, items);
         gv.setAdapter(gridAdapter);
-        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
-                Toast.makeText(getApplicationContext(),
-                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
-            }
-        });
 
-    }
 
-    public void itemClicked(int position) {
-        text.setText(items[position]);
     }
 
 
